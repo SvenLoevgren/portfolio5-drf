d6774411c96fcda1fe7d228ba4fa69a481b40740
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-07-17 00:06:26 +0000

    allowed host adjusted again to match heroku generated url

--- django_project_fastfood/settings.py
+++ django_project_fastfood/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fastfood-drf.herokuapp.com", "localhost", "8000-svenloevgre-portfolio5d-1in2rrtca15.ws-eu101.gitpod.io"]
+ALLOWED_HOSTS = ["fastfood-drf-dfd5756f86e9.herokuapp.com", "localhost", "8000-svenloevgre-portfolio5d-1in2rrtca15.ws-eu101.gitpod.io"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://github.com',
