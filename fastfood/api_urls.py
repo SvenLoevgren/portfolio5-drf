57b917f93660467bba94350324da39d534853ed8
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-08 14:14:49 +0000

    Api endpoint change path to match react app

--- fastfood/api_urls.py
+++ fastfood/api_urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('bookings/', BookingListCreateAPIView.as_view(), name='booking-list'),
     path('bookings/<int:pk>/', BookingDetailAPIView.as_view(), name='booking-detail'),
-    path('menu/', MenuAPIView.as_view(), name='menu-list'), # for REACT app
+    path('menu/cart', MenuAPIView.as_view(), name='menu-list'), # for REACT app
     path('api-token-auth/', obtain_auth_token, name='api-token-auth'), # for REACT app
 ]
