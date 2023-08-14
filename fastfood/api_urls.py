f596d68c296c88c84ccacc472da91bc679f6bf61
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-14 16:52:17 +0000

    rollback preformed to correct delete item urls

--- fastfood/api_urls.py
+++ fastfood/api_urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('menu/', MenuAPIView.as_view(), name='menu-list'), # for REACT app
-    path('menu/item/delete/', MenuItemDeleteView.as_view(), name='menu-item-delete'),
+    path('menu/item/<item_ids>/delete/', MenuItemDeleteView.as_view(), name='menu-item-delete'),
 ]
