0be1a3434a39353598567e8fbe7bf81766c57379
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-17 15:36:59 +0000

    The order of calling restful api's changed in api_urls.py for easy maintenance and best practie

--- fastfood/api_urls.py
+++ fastfood/api_urls.py
@@ -12,8 +12,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('menu/', MenuAPIView.as_view(), name='menu-list'), 
+    path('menu/', MenuAPIView.as_view(), name='menu-list'),
+    path('menu/items/create/', MenuItemCreateView.as_view(), name='menu-item-create'),
     path('menu/item/<int:pk>/delete/', MenuItemDeleteView.as_view(), name='menu-item-delete'),
     path('menu/item/<int:pk>/update/', MenuItemUpdateView.as_view(), name='menu-item-update'),
-    path('menu/items/create/', MenuItemCreateView.as_view(), name='menu-item-create'),
+
 ]
